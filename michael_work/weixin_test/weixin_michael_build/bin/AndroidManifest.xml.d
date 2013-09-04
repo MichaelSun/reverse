8715eedfc749b0f62aa26db798467096142c22f3
guoqing.sun <guoqing.sun@renren-inc.com>
2013-09-05 07:38:36 +0800

    增加初始编译资源，目前能够编译过，会crash

---
+++ michael_work/weixin_test/weixin_michael_build/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/michael/github/reverse/michael_work/weixin_test/weixin_michael_build/bin/AndroidManifest.xml : \
+/Users/michael/github/reverse/michael_work/weixin_test/weixin_michael_build/AndroidManifest.xml \
---
+++ michael_work/weixin_test/weixin_michael_build/bin/weixin_test-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/michael/github/reverse/michael_work/weixin_test/weixin_michael_build/bin/weixin_test-release-unsigned.apk : \
+/Users/michael/github/reverse/michael_work/weixin_test/weixin_michael_build/bin/weixin_test.ap_ \
+/Users/michael/github/reverse/michael_work/weixin_test/weixin_michael_build/bin/classes.dex \
+/Users/michael/github/reverse/michael_work/weixin_test/android-sdk-mac_x86/tools/support/annotations.jar \
